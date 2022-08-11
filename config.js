345e4fd98d32e95020132750477177ee10db7bfa
Bisnisokteto <110271297+Bisnisokteto@users.noreply.github.com>
2022-08-11 13:34:24 +0700

    Usus.js
    Hs

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6281511480762'],
-  ['6281511480762', 'YahyaGanzz', true]
+  ['6288223674569'],
+  ['6288223674569', 'Xynn-MD', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -41,8 +41,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'AzzBot-Md'
-  var sticker_author = 'YahyaGanzz'
+  var sticker_name = 'Xynn-MD'
+  var sticker_author = 'DikaOfc'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'AzzBot-Yahya'
+global.wm = 'Xynn-DikaOfc'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
